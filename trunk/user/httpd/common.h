d1cb842502c2a48e4e255653fc57bf244dd81047
HiGarfield <HiGarfield@126.com>
2026-09-15 00:44:02 +0800

    fix(httpd): EVM bit masks shift past type width + move to free bits 16 and 46

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -105,9 +105,9 @@ typedef u_int8_t u8;
 #define EVM_RESTART_DDNSTO	    	(1ULL << 61)
 #define EVM_RESTART_WIREGUARD		(1ULL << 62)
 #define EVM_RESTART_ALDRIVER		(1ULL << 63)
-#define EVM_RESTART_REBOOT			(1ULL << 64)
+#define EVM_RESTART_REBOOT			(1ULL << 16)
 
-#define EVM_BLOCK_UNSAFE			(1ULL << 65) /* special case */
+#define EVM_BLOCK_UNSAFE			(1ULL << 46) /* special case */
 
 
 #define EVT_RESTART_FIREWALL		1
